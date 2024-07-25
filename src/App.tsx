ad4849019ebf8b77e6fe0c5de4860b45b00fab34
LuisPrendas <luis.prendas@moonhicr.com>
2024-07-25 12:04:11 -0700

    New name

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <main className='w-full h-screen flex flex-col items-center justify-center gap-8 bg-slate-900 text-gray-200 pb-60'>
       <div className='flex flex-col items-center justify-center'>
-        <h1 className='text-9xl font-bold'>K<span className='text-yellow-400'>ui</span>zz</h1>
+        <h1 className='text-9xl font-bold'>Luis <span className='text-yellow-400'>Daniel</span> Prendas</h1>
         <h2 className='text-xl'>Luis Prendas Chavarria</h2>
       </div>
 
